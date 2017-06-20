86c1268aea2d08d33453051e57b1e93a95011177
Pauric Carroll <pauriccarroll@google.com>
2017-06-20 11:20:36 +1000

    Initial commit

---
+++ app/src/main/java/com/example/android/project3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.project3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openResultList(View view) {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+}
